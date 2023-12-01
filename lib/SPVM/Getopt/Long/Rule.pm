06814e0ee8a485170b30ada0e6da38e4c5f7ecb7
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-12-01 13:35:34 +0900

    Imprlment part.

---
+++ lib/SPVM/Getopt/Long/Rule.pm
@@ -0,0 +1,20 @@
+package SPVM::Getopt::Long::Rule;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::Getopt::Long::Rule - Rule to Parse Command Line Options
+
+=head1 Description
+
+The Getopt::Long::Rule class of L<SPVM> represents the rule to parse command line options.
+
+=head1 Copyright & License
+
+Copyright (c) 2023 Yuki Kimoto
+
+MIT License
+
